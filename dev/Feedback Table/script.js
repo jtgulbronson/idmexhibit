0bee85f727f4e151e925423b7f811a7bdc28ab1c
Jordan Gulbronson <jordangulbronson11@gmail.com>
2017-11-07 12:42:41 -0500

    Working color mixer plugin

--- dev/Feedback Table/script.js
+++ dev/Feedback Table/script.js
@@ -1,23 +1,31 @@
 // Setting up colors array
 var colors = [];
-//Setting up color mixing result
-var result_color;
 
 //Setting up Hammer.js with jQuery
 var toTap = $(".spice");
 toTap.hammer().on("tap", function(ev) {
     //Making currentHex the name value of input.js
-    let currentHex = this.attributes["name"].value;
-    //Checking for Hammer.js tap
-    console.log("tap detected");
+    let currentHex = eval(this.attributes["name"].value);
     //Logging name value
     console.log(currentHex);
     //Pushing name value to colors array
     colors.push(currentHex);
     //Checking array values
     console.log(colors);
+    return colors;
+});
+
+console.log(colors);
+//Setting up Hammer.js with jQuery
+var toTapBtn = $("#mix-colors");
+toTapBtn.hammer().on("tap", function(ev) {
     //Mixing colors through Color_mixer https://github.com/AndreasSoiron/Color_mixer
-    var result_color = Color_mixer.mix(colors);
-    //Checking color mixing result
-    console.log(result_color);
+    if (colors.length < 1 ) {
+        $("#test-strip").css("background-color","#ffffff");
+    } else {
+    result_color = Color_mixer.mix(colors);
+    $("#test-strip").css("background-color",result_color.toHexString());
+
+    colors = [];
+    }
 });
