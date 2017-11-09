9cd59cc5db00d67b1a1bde4eeed4a6bf50473bae
Jordan Gulbronson <jordangulbronson11@gmail.com>
2017-11-09 17:04:39 -0500

    Updating Socket.io to broadcast and just emit

---
+++ dev/textile-table/server.js
@@ -0,0 +1,33 @@
+var express = require('express'),
+    http = require('http');
+
+//setting root
+var app = express();
+app.use(express.static(__dirname + "/"));
+
+//setting the PORT, will be determined by Heroku or set to 5000 on local machine
+var port = process.env.PORT || 5000;
+
+var server = http.createServer(app);
+server.listen(port);
+
+console.log("http server listening on %d", port);
+
+var io = require('socket.io')(server);
+
+console.log("Socket.io Server was created");
+
+io.sockets.on('connection', function (socket) {
+    console.log("socket connected");
+
+    // console.log(socket);
+    socket.on('myTap', function (data) {
+        console.log(data);
+        socket.emit('addThread', data);
+    });
+    socket.on('toTextile', function (dataTwo) {
+        console.log(dataTwo);
+        socket.broadcast.emit('toTextile', dataTwo);
+    });
+
+});
