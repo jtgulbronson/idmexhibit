f3156a4b1a3c34c870c522148317fca17e04e5af
Jackelyn Kjerstie <jackelynkjerstie@gmail.com>
2017-11-27 18:26:30 -0500

    gallery work - thanksgiving week

--- dev/gallery/js/init.js
+++ dev/gallery/js/init.js
@@ -1,77 +1,99 @@
-$(function(){
+$( document ).ready(function(){
 
-       // target elements with the "draggable" class
-   interact('#photo_1')
+/////////////////////////////////////
+// Drag and Drop
+////////////////////////////////////
+
+// Draggable
+   interact('.example_block')
     .draggable({
-      // enable inertial throwing
-      inertia: false,
-      // keep the element within the area of it's parent
+      inertia: false, //makes the element stop when user stops
+      // keep the element within the area of it's parent -- doesnt work??
       restrict: {
         drag: 'parent',
         endOnly: true,
         elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
       },
-      // enable autoScroll
-      autoScroll: true,
-
-      // call this function on every dragmove event
-      onmove: dragMoveListener,
+      autoScroll: false,  // disable scroll via drag
+      onmove: dragHandler, // call this function on every dragmove event
+    });
+    function dragHandler (dragOne) {
+      var target = dragOne.target;
+        x = (parseFloat(target.getAttribute('data-x')) || 0) + dragOne.dx,
+        y = (parseFloat(target.getAttribute('data-y')) || 0) + dragOne.dy;
+      	target.style.top = y + 'px'; //adjust positioning based on dragged location
+      	target.style.left = x + 'px'; //adjust positioning based on dragged location
+	      // update the position attributes
+	      target.setAttribute('data-x', x);
+	      target.setAttribute('data-y', y);
+	      // document.getElementById('transform-info').textContent = 'position = ' + x + ',' + y;
+		  // used for debugging - sometimes the cards like to run away
+      dragOne.preventDefault();
+    };
+// enable draggables to be dropped into this
+    interact('.dropzone').dropzone({
+      // only accept elements matching this CSS selector
+      accept: '.example_block',
+      // Require a 75% element overlap for a drop to be possible
+      overlap: 0.25,
+      ondragenter: function (event) {
+        var draggableElement = event.relatedTarget,
+        dropzoneElement = event.target;
+        draggableElement.classList.add('dropped-element');
+        draggableElement.classList.remove('removed-element');
+        var id = draggableElement.getAttribute('id');
+        switch(id){
+          case 'photo_1':
+          $('#photo_1_text').css('display', 'block');
+          break;
+          case 'photo_2':
+          $('#photo_2_text').css('display', 'block');
+          break;
+        }
+      },
+      ondragleave: function (event) {
+        var draggableElement = event.relatedTarget,
+        dropzoneElement = event.target;
+         draggableElement.classList.add('removed-element');
+        draggableElement.classList.remove('dropped-element');
+        var id = draggableElement.getAttribute('id');
+        switch(id){
+          case 'photo_1':
+          $('#photo_1_text').css('display', 'none');
+          break;
+          case 'photo_2':
+          $('#photo_2_text').css('display', 'none');
+          break;
+        }
+      }
     });
 
-    function dragMoveListener (event) {
-      $('#photo_1').css('width','300px');
-      $('#photo_1').css('height', '150px');
-      $('#photo_1').css('background-position', 'center center');
-      $('#photo_1').css('background', '#28464B');
-      $('#photo_1').css('padding-bottom','0px');
-      $('#photo_1_text').css('display', 'none');
-      var target = event.target,
+    // Rotatable
+      var angle = 0; // used for rotation
+      interact('#photo_1').gesturable({
+        onmove: function (rotateOne) {
+          var rotateObj = document.getElementById('photo_1'); //find our object to rotate
+          angle += rotateOne.da; //track our angle changed by user
+          rotateObj.style.webkitTransform = rotateObj.style.transform = 'rotate(' + angle + 'deg)';
+          //set our rotation from user input - for old and new browsers
 
-          // keep the dragged position in the data-x/data-y attributes
-          x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-          y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+          // document.getElementById('transform-info2').textContent = 'Rotation = ' + angle.toFixed(2) + '°';
+          //Used for debugging - the cards like to run away without warning
+          }
+      });
+      //Rotatable
+      var angle = 0;
+        interact('#photo_2').gesturable({
+          onmove: function (rotateTwo) {
+            var rotateObj = document.getElementById('photo_2'); //find our object to rotate
+            angle += rotateTwo.da; //track our angle changed by user
+            rotateObj.style.webkitTransform = rotateObj.style.transform = 'rotate(' + angle + 'deg)';
+            //set our rotation from user input - for old and new browsers
 
-      // translate the element
-      // target.style.webkitTransform =
-      // target.style.transform =
-      //   'translate(' + x + 'px, ' + y + 'px)';
-      target.style.top = y + 'px';
-      target.style.left = x + 'px';
+            // document.getElementById('transform-info2').textContent = 'Rotation = ' + angle.toFixed(2) + '°';
+            //Used for debugging - the cards like to run away without warning
+            }
+        });
 
-      // update the position attributes
-      target.setAttribute('data-x', x);
-      target.setAttribute('data-y', y);
-      document.getElementById('transform-info').textContent =
-      'position = ' + x + ',' + y;
 
-    };
-
-    interact('#photo_1')
-    .on('tap', function (event) {
-        $('#photo_1').css('width','600px');
-        $('#photo_1').css('height', '400px');
-        $('#photo_1').css('padding-bottom','100px');
-        $('#photo_1').css('background-position', 'top left');
-        $('#photo_1').css('background', '#28464B');
-        $('#photo_1_text').css('display', 'block');
-        event.preventDefault();
-     });
-
-    var angle = 0;
-
-  interact('#photo_1').gesturable({
-    onmove: function (event) {
-      var arrow = document.getElementById('photo_1');
-
-      angle += event.da;
-
-      arrow.style.webkitTransform =
-      arrow.style.transform =
-        'rotate(' + angle + 'deg)';
-
-        document.getElementById('transform-info').textContent =
-        'Rotation = ' + angle.toFixed(2) + '°';
-      }
-  });
-
-});
+});//do not lose this closing bracket - unless you like breaking everything for fun
