0ce14c2fc3e19fd6211202e17086cb78a55b5754
Jackelyn Kjerstie <jackelynkjerstie@gmail.com>
2018-01-20 20:45:16 -0500

    browser resets after no interaction for 2 min

--- dev/magnifying-gallery/js/init.js
+++ dev/magnifying-gallery/js/init.js
@@ -99,4 +99,18 @@ var glassTwo = $('#glass_2');
       }
     });
 
+    var time = new Date().getTime();
+     $(document.body).bind("touchmove mousemove keypress", function(e) {
+         time = new Date().getTime();
+     });
+
+     function refresh() {
+         if(new Date().getTime() - time >= 120000)
+             window.location.reload(true);
+         else
+             setTimeout(refresh, 20000);
+     }
+
+     setTimeout(refresh, 20000);
+
 });//do not lose this closing bracket - unless you like breaking everything for fun
