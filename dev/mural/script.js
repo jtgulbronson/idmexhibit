14d2246896e0f60be39c10c23b7c1a20c4f95e5b
angel wu <angelwu273@gmail.com>
2018-05-22 11:01:32 -0400

    added in reticle and edited videos

--- dev/mural/script.js
+++ dev/mural/script.js
@@ -1,36 +1,138 @@
-//VARIABLES
-var vidL = document.getElementById("cart-L-vid");
-var vidM = document.getElementById("cart-M-vid");
-var vidR = document.getElementById("cart-R-vid");
+//VIDEO TRANSITIONS
+$(document).ready(function(){
+	var delay = 500, setTimeoutConst;
+	
+	$("#cart-L-container").hover(function(){
+		setTimeoutConst = setTimeout(function(){
+			
+			$("#cart-L-vid").css({
+				transition: '1s'
+			})
+			
+		}, delay);
+	}, function(){
+		//document.getElementById("exposure-L").style.visibility = "hidden";
+		clearTimeout(setTimeoutConst);
+	});
+	
+	$("#cart-M-container").hover(function(){
+		setTimeoutConst = setTimeout(function(){
+			
+			$("#cart-M-vid").css({
+				transition: '1s'
+			})
+			
+		}, delay);
+	}, function(){
+		clearTimeout(setTimeoutConst);
+	});
+	
+	$("#cart-R-container").hover(function(){
+		setTimeoutConst = setTimeout(function(){
+			
+			$("#cart-R-vid").css({
+				transition: '1s'
+			})
+			
+		}, delay);
+	}, function(){
+		clearTimeout(setTimeoutConst);
+	});
+	
+	//EXIT VIDEOS
+	$(".vid-btn").hover(function(){
+		setTimeoutConst = setTimeout(function(){
+			
+			$("#cart-L-vid").css({
+				transition: '1s',
+				display: 'none'
+			})
+			
+			$("#cart-M-vid").css({
+				transition: '1s',
+				display: 'none'
+			})
+			
+			$("#cart-R-vid").css({
+				transition: '1s',
+				display: 'none'
+			})
+			
+		}, delay);
+	}, function(){
+		clearTimeout(setTimeoutConst);
+	})
+	
+});
 
-//videos appear and start playing when carts are zoomed in
-function displayVidL(){
+//PLAY AND STOP VIDEOS
+var lVid = document.getElementById("left-vid");
+var mVid = document.getElementById("mid-vid");
+var rVid = document.getElementById("right-vid");
+
+function playVidL(){
 	document.getElementById("cart-L-vid").style.display = "block";
-	document.getElementById("cart-M-vid").style.display = "none";
-	document.getElementById("cart-R-vid").style.display = "none";
-	vidL.play();
+	lVid.play();
 }
 
-function displayVidM(){
+function playVidM(){
 	document.getElementById("cart-M-vid").style.display = "block";
-	document.getElementById("cart-L-vid").style.display = "none";
-	document.getElementById("cart-R-vid").style.display = "none";
-	vidM.play();
+	mVid.play();
 }
 
-function displayVidR(){
+function playVidR(){
 	document.getElementById("cart-R-vid").style.display = "block";
-	document.getElementById("cart-L-vid").style.display = "none";
-	document.getElementById("cart-M-vid").style.display = "none";
-	vidR.play();
+	rVid.play();
 }
 
-//videos stop playing and disappear when zoomed out
-function hideVid(){
+function stopVidL(){
 	document.getElementById("cart-L-vid").style.display = "none";
+	lVid.pause();
+}
+
+function stopVidM(){
 	document.getElementById("cart-M-vid").style.display = "none";
+	mVid.pause();
+}
+
+function stopVidR(){
 	document.getElementById("cart-R-vid").style.display = "none";
-//	vidL.pause();
-//	vidM.pause();
-//	vidR.pause();
+	rVid.pause();
+}
+
+document.getElementById("left-vid").addEventListener("ended", function(){
+	document.getElementById("cart-L-vid").style.display = "none";
+});
+
+document.getElementById("mid-vid").addEventListener("ended", function(){
+	document.getElementById("cart-M-vid").style.display = "none";
+});
+
+document.getElementById("right-vid").addEventListener("ended", function(){
+	document.getElementById("cart-R-vid").style.display = "none";
+});
+
+//RETICLE - CONTROL OTHER FUNCTIONS
+function reticleOnL(){
+  document.getElementById("reticle-L").style.display = "block";
+}
+
+function reticleOffL(){
+  document.getElementById("reticle-L").style.display = "none";
+}
+
+function reticleOnM(){
+  document.getElementById("reticle-M").style.display = "block";
+}
+
+function reticleOffM(){
+  document.getElementById("reticle-M").style.display = "none";
+}
+
+function reticleOnR(){
+  document.getElementById("reticle-R").style.display = "block";
+}
+
+function reticleOffR(){
+  document.getElementById("reticle-R").style.display = "none";
 }
